dc7d72a36b9a19b052ba811c62d9242525e62ad0
Mehdi Cherif <mehdi.cherif@insat.ucar.tn>
2023-06-29 12:29:57 +0100

    fixing some things

--- src/Form.js
+++ src/Form.js
@@ -10,19 +10,20 @@ function Form(){
 
         const docRef = await setDoc(doc(db, "members", new Date().toString()), data);
     }
+    const clickHandler = (button)=>{button.target.classList.toggle("move")}
     return(
         <div  className="form-group">
             <form onSubmit={handleSubmit(onSubmit)} className="form">
                 <div className="formLabels">
                     <label className="label">Prénom :</label>
-                    <input className="input"  {...register("prenom")} /></div>
+                    <input placeholder="Prénom" className="input"  {...register("prenom")} /></div>
                 <div className="formLabels">
-                    <label className="label">Nom :</label>
-                    <input className="input" {...register("nom", {required: true})} />
+                    <label  className="label">Nom :</label>
+                    <input placeholder="Nom" className="input" {...register("nom", {required: true})} />
                     {errors.exampleRequired && <span style={{color:"white"}}>This field is required</span>}</div>
                 <div className="formLabels">
                     <label className="label">Email :</label>
-                    <input className="input" {...register("email", {
+                    <input  placeholder="Email" className="input" {...register("email", {
                         required: true,
                         pattern: {
                             value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
@@ -32,8 +33,8 @@ function Form(){
                     {errors.exampleRequired && <span style={{color:"white"}}>This field is required</span>}
                     {errors.email && <span style={{color:"white"}}>{errors.email.message}</span>}</div>
                 <div className="formLabels">
-                    <label className="label">Numéro de Téléphone :</label>
-                    <input className="input" {...register("phone", {
+                    <label  className="label">Numéro de Téléphone :</label>
+                    <input placeholder="Numéro Téléphone" className="input" {...register("phone", {
                         required: true,
                         pattern: {
                             value: /^[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{5}$/,
@@ -43,32 +44,32 @@ function Form(){
                     {errors.exampleRequired && <span style={{color:"white"}}>This field is required</span>}
                     {errors.phone && <span style={{color:"white"}}>{errors.phone.message}</span>}</div>
                 <div className="formLabels">
-                    <label className="label">Lien Facebook :</label>
-                    <input className="input"  {...register("fb",{
+                    <label  className="label">Lien Facebook :</label>
+                    <input placeholder="Lien Facebook" className="input"  {...register("fb",{
                         required: true,
                         pattern: {
                             value: /^(https?:\/\/)?(www\.)?facebook.com\/[a-zA-Z0-9(\.\?)?]/,
                         }})} /></div>
                 <div className="formLabels">
                     <label className="label">Vos intérets :</label>
-                    <input className="input"  {...register("interests")} /></div>
+                    <input placeholder="interets" className="input"  {...register("interests")} /></div>
                 <div className="formLabels">
                     <label className="label">Vos attentes :</label>
-                    <input className="input"  {...register("expectations")} /></div>
+                    <input  placeholder="attentes" className="input"  {...register("expectations")} /></div>
                 <div className="formLabels">
                     <div className="filliere">
 
                         <div className="fillabel">
                             <label className="label">Votre Niveau :</label>
-                            <label className="label"> Filiere :</label></div>
-                        <div className="fil"><select className="input" {...register("filiere")}>
+                            <label  className="label"> Filiere :</label></div>
+                        <div className="fil"><select defaultValue="1" className="input" {...register("niveau")}>
                             <option value="1">1</option>
                             <option value="2">2</option>
                             <option value="3">3</option>
                             <option value="4">4</option>
                             <option value="5">5</option>
                         </select>
-                            <select className="input" {...register("filiere")}>
+                            <select defaultValue="MPI" className="input" {...register("filiere")}>
                                 <option value="GL">GL</option>
                                 <option value="MPI">MPI</option>
                                 <option value="CBA">CBA</option>
@@ -78,7 +79,7 @@ function Form(){
                                 <option value="CH">CH</option>
                                 <option value="BIO">BIO</option>
                             </select></div></div></div>
-                <input className="sub" type="submit" value="Submit"/>
+                <input onMouseOver={clickHandler} className="sub stop" type="submit" value="Submit"/>
             </form>
         </div>);
 }
