dda650410efbaeb5b05bb34ea2ca829895e92323
Mehdi Cherif <mehdi.cherif@insat.ucar.tn>
2023-07-06 12:29:48 +0100

    security updates

--- src/Form.js
+++ src/Form.js
@@ -31,14 +31,14 @@ function Form(){
             <form onSubmit={handleSubmit(onSubmit)} className="form" id="f">
                 <div className="formLabels">
                     <label className="firstLabel">Prénom :</label>
-                    <motion.input whileHover={{scale:1.2}} placeholder="Prénom" className="firstInput"  {...register("prenom")} /></div>
+                    <motion.input whileFocus={{scale:1.2}}  placeholder="Prénom" className="firstInput"  {...register("prenom")} /></div>
                 <div className="formLabels">
                     <label  className="label">Nom :</label>
-                    <motion.input whileHover={{scale:1.2}} placeholder="Nom" className="input" {...register("nom", {required: true})} />
+                    <motion.input whileFocus={{scale:1.2}}  placeholder="Nom" className="input" {...register("nom", {required: true})} />
                     {errors.exampleRequired && <span style={{color:"white"}}>This field is required</span>}</div>
                 <div className="formLabels">
                     <label className="label">Email :</label>
-                    <motion.input whileHover={{scale:1.2}}  placeholder="Email" className="input" {...register("email", {
+                    <motion.input whileFocus={{scale:1.2}}  placeholder="Email" className="input" {...register("email", {
                         required: true,
                         pattern: {
                             value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
@@ -49,7 +49,7 @@ function Form(){
                     {errors.email && <span style={{color:"white"}}>{errors.email.message}</span>}</div>
                 <div className="formLabels">
                     <label  className="label">Numéro de Téléphone :</label>
-                    <motion.input whileHover={{scale:1.2}} placeholder="Numéro Téléphone" className="input" {...register("phone", {
+                    <motion.input whileFocus={{scale:1.2}} placeholder="Numéro Téléphone" className="input" {...register("phone", {
                         required: true,
                         pattern: {
                             value: /^[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{5}$/,
@@ -60,31 +60,31 @@ function Form(){
                     {errors.phone && <span style={{color:"white"}}>{errors.phone.message}</span>}</div>
                 <div className="formLabels">
                     <label  className="label">Lien Facebook :</label>
-                    <motion.input whileHover={{scale:1.2}} placeholder="Lien Facebook" className="input"  {...register("fb",{
+                    <motion.input whileFocus={{scale:1.2} } placeholder="Lien Facebook" className="input"  {...register("fb",{
                         required: true,
                         pattern: {
                             value: /^(https?:\/\/)?(www\.)?facebook.com\/[a-zA-Z0-9(\.\?)?]/,
                         }})} /></div>
                 <div className="formLabels">
                     <label className="label">Vos intérêts :</label>
-                    <motion.input whileHover={{scale:1.2}} placeholder="intérêts" className="input"  {...register("interests")} /></div>
+                    <motion.input whileFocus={{scale:1.2}}  placeholder="intérêts" className="input"  {...register("interests")} /></div>
                 <div className="formLabels">
                     <label className="label">Vos attentes :</label>
-                    <motion.input whileHover={{scale:1.2}}  placeholder="attentes" className="input"  {...register("expectations")} /></div>
+                    <motion.input whileFocus={{scale:1.2}}  placeholder="attentes" className="input"  {...register("expectations")} /></div>
                 <div className="formLabels">
                     <div className="filliere">
 
                         <div className="fillabel">
                             <label className="label">Votre Niveau :</label>
                             <label  className="label"> Filière :</label></div>
-                        <div className="fil"><motion.select whileHover={{scale:1.2}} defaultValue="1" className="input" {...register("niveau")}>
+                        <div className="fil"><motion.select whileFocus={{scale:1.2}} defaultValue="1" className="input" {...register("niveau")}>
                             <option value="1">1</option>
                             <option value="2">2</option>
                             <option value="3">3</option>
                             <option value="4">4</option>
                             <option value="5">5</option>
                         </motion.select>
-                            <motion.select whileHover={{scale:1.2}} defaultValue="MPI" className="input" {...register("filiere")}>
+                            <motion.select whileFocus={{scale:1.2}} defaultValue="MPI" className="input" {...register("filiere")}>
                                 <option value="GL">GL</option>
                                 <option value="MPI">MPI</option>
                                 <option value="CBA">CBA</option>
